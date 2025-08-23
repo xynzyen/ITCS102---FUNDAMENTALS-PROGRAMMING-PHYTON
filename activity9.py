9d7c892459e2921dd1eb1e26fc2e248c7e4b992d
xynzyen <cabangonjillienezeth@gmail.com>
2025-08-23 09:31:05 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,8 @@
+username = 'yenyen'
+password = 'jhapikit26'
+
+print(username == 'christian')  # True
+print(password == 'pogiako405')  # True
+print((username == 'christian') or (password == 'pogiako405'))  # True
+print((username == 'christian') and (password == 'pogiako405'))
+print(not((username == 'christian') and (password == 'pogiako405')))
