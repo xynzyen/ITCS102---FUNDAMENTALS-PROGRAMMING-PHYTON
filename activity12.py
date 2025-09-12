c1f830d06dfdd0628abf31b515ccf9bae1459401
xynzyen <cabangonjillienezeth@gmail.com>
2025-09-12 11:26:35 +0800

    Create activity12.py

---
+++ activity12.py
@@ -0,0 +1,18 @@
+print("hello Yen")
+for i in range(1, 11, 1):
+    print(i,"ganda mo yen")
+
+for i in range(1, 11, 1):
+    print(i,"gutom na si yen")
+
+print("with step")
+for i in range(1, 11, 2):
+    print(i,"ganda mo yen")
+
+print("without step")
+for i in range(1, 11):
+    print(i,"ganda mo yen")
+
+print("without start and step")
+for i in range(11):
+    print(i,"ganda mo yen")
