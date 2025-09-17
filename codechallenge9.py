eca9477615fa3929c94cdbcba04f209bbd2b0756
xynzyen <cabangonjillienezeth@gmail.com>
2025-09-17 15:22:01 +0800

    codechallenge9.py

---
+++ codechallenge9.py
@@ -0,0 +1,12 @@
+#countdown timer 
+
+print("\nCOUNTDOWN TIMER⏰")
+
+#ask the user for the starting number 
+start = eval(input("Enter starting number for countdown: "))
+print("\nCountdown Started:")
+
+#Loop Backwards from start downto 1 
+for i in range (start, 0, -1):
+    print(i)
+print("LIFT OFF!🎇")
