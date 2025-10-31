6c785601a8c97bafc5a9ec25273da33bac7efb01
xynzyen <cabangonjillienezeth@gmail.com>
2025-10-31 18:25:59 +0800

    Update and rename activity4.py to activity04.py

---
+++ activity04.py
@@ -0,0 +1,3 @@
+
+name = input("Enter a string -> ")
+print("Your name has ", len(name), "character")
--- activity4.py
+++
@@ -1,3 +0,0 @@
-
-name = input("Enter a string -> ")
-print("Your name has ", len(name), "character")
