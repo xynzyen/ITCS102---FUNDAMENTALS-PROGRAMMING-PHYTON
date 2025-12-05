82aa9d99f3d6a9d01cf3c9ba7751a5a69eaf7f0e
xynzyen <cabangonjillienezeth@gmail.com>
2025-12-05 20:45:59 +0800

    Create activity25_1.py

---
+++ activity25_1.py
@@ -0,0 +1,18 @@
+
+def activity1():
+    print("Hi, I am Yen Yen, student from the BSIT First Year")
+    print("Section 1A")
+    print("This is my first file in my REPO.")
+    print("Nakakagutiom")
+    print("About Me. :")
+    print("From Brgy Ibabang Dupay Lucena City, I love singing and playing guitar.")
+
+def activity2():
+    name = input("Input your name: ")
+    print("Hi ",name," Welcome to the matrix.")
+def activity3():
+    print("Happy \t Monday")
+    print("BSIT 1A from DLL")
+def activity4():
+    name = input("Enter a string -> ")
+    print("Your name has ", len(name), "character")
