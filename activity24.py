9183b53b976d7595c1b83f28483ab92f0cd49833
xynzyen <cabangonjillienezeth@gmail.com>
2025-12-05 20:33:43 +0800

    Create activity24.py

---
+++ activity24.py
@@ -0,0 +1,23 @@
+
+
+def greeter(name):
+    print(f"Hi, {name} pogi ")
+
+def summation(a):
+    sum = 0
+    for b in range(a, 0, -1):
+        sum += b
+    print(f'So The sum of {a} is {sum}')
+
+greeter("jhared")
+greeter("loriaga jhared")
+greeter("pogi jhared")
+greeter("jha pogi")
+
+
+
+summation(1)
+summation(2)
+summation(11)
+summation(12)
+summation(15)
