3c5c25293b8af10a699757c3b658a81797b603cd
xynzyen <cabangonjillienezeth@gmail.com>
2025-12-07 11:36:27 +0800

    Create codechallenge10.py

---
+++ codechallenge10.py
@@ -0,0 +1,11 @@
+
+
+
+for i in range(11,0,-1):
+  for x in range(1, i, 1):
+    print(" ", end= ' ')
+  for y in range(10, i, -1):
+    print("*",end = ' ')
+  for i in range(10,i,-1):
+    print("*",end = ' ') 
+  print()
