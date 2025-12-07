e91bfcb2066e4feb9ceb46d36302087612e9271d
xynzyen <cabangonjillienezeth@gmail.com>
2025-12-07 11:39:21 +0800

    Create codechallenge13.py

---
+++ codechallenge13.py
@@ -0,0 +1,58 @@
+
+
+for i in range(1,3,1):
+    for i in range(15,i,-1):
+        print(" ",end=" ") 
+    for x in range(0,i,1):
+        print("*",end=" ") 
+    for x in range(1,i,1):
+        print("*",end=" ")
+    for i in range(10,i,-1):
+        print(" ",end=" ")
+    print()
+for i in range(3,0,-1):
+    for i in range(15,i,-1):
+        print(" ",end=" ") 
+    for x in range(0,i,1):
+        print("*",end=" ")
+    for x in range(1,i,1):
+        print("*",end=" ")
+    for i in range(10,i,-1):
+        print(" ",end=" ")
+    print()
+for i in range(1,7,1):
+    for i in range(15,i,-1):
+        print(" ",end=" ") 
+    for x in range(0,i,1):
+        print("*",end=" ") 
+    for x in range(1,i,1):
+        print("*",end=" ")
+    for i in range(10,i,-1):
+        print(" ",end=" ")
+    print()
+for i in range(1,13,1):
+    for i in range(15,i,-1):
+        print(" ",end=" ") 
+    for x in range(0,i,1):
+        print("*",end=" ") 
+    for x in range(1,i,1):
+        print("*",end=" ")
+    for i in range(10,i,-1):
+        print(" ",end=" ")
+    print()
+for i in range(1,15,1):
+    for i in range(15,i,-1):
+        print(" ",end=" ") 
+    for x in range(0,i,1):
+        print("*",end=" ") 
+    for x in range(1,i,1):
+        print("*",end=" ")
+    for i in range(10,i,-1):
+        print(" ",end=" ")
+    print()
+for i in range(1,11,1):
+    for i in range(11,1,-1):
+        print(" ",end=" ") 
+    for x in range(0,9,1):
+        print("*",end=" ") 
+    print()
